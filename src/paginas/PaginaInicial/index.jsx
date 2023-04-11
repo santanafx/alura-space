b2878314d19b8ec1e90878d5e7c9689616e3cbf7
Lucas Santana Figueiredo <santanafx@hotmail.com>
2023-04-11 14:22:38 -0300

    Completando pagina e alterando configs globais

--- src/paginas/PaginaInicial/index.jsx
+++ src/paginas/PaginaInicial/index.jsx
@@ -1,10 +1,22 @@
 import React from "react";
 import { Cabecalho } from "../../componentes/Cabecalho";
+import Menu from "../../componentes/Menu";
+import banner from "./banner.png";
+import styles from "./paginaInicial.module.scss";
 
 export const PaginaInicial = () => {
   return (
     <div>
       <Cabecalho />
+      <main>
+        <section className={styles.principal}>
+          <Menu />
+          <div className={styles.principal__imagem}>
+            <h1>A galeria mais completa do espaço</h1>
+            <img src={banner} alt="A imagem da terra vista do espaço" />
+          </div>
+        </section>
+      </main>
     </div>
   );
 };
